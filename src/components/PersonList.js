48d16866c36a4eec1db010b6bfeaa535882c8fc6
JakeM822 <jakemedica@gmail.com>
2026-07-08 15:30:39 +0000

    Sync package-lock.json

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -7,7 +7,7 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://q76xnqzf38.execute-api.us-west-2.amazonaws.com/prod/get-person`)
+    axios.get('https://rhru8at9ud.execute-api.us-west-2.amazonaws.com/test/to-do')
       .then(res => {
         const persons = res.data.body;
         this.setState({ persons });
